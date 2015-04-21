eb8547b2ce4aa08bdb1012ce497a28a0a8ef5ccd
ColColonCleaner <danielgradinjan@gmail.com>
2015-04-21 01:09:56 -0500

    Version 6.6.0.0

--- AdKats.cs
+++ AdKats.cs
@@ -20,11 +20,11 @@
  * Development by Daniel J. Gradinjan (ColColonCleaner)
  * 
  * AdKats.cs
- * Version 6.5.9.9
+ * Version 6.6.0.0
  * 21-APR-2015
  * 
  * Automatic Update Information
- * <version_code>6.5.9.9</version_code>
+ * <version_code>6.6.0.0</version_code>
  */
 
 using System;
@@ -64,7 +64,7 @@ namespace PRoConEvents
     public class AdKats : PRoConPluginAPI, IPRoConPluginInterface
     {
         //Current Plugin Version
-        private const String PluginVersion = "6.5.9.9";
+        private const String PluginVersion = "6.6.0.0";
 
         public enum GameVersion
         {
