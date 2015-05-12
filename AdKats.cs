4a27e8f1e0a0803fa1ca599a27dd18562289dd88
ColColonCleaner <danielgradinjan@gmail.com>
2015-05-11 20:50:33 -0500

    Version 6.6.9.5

--- AdKats.cs
+++ AdKats.cs
@@ -20,11 +20,11 @@
  * Development by Daniel J. Gradinjan (ColColonCleaner)
  * 
  * AdKats.cs
- * Version 6.6.9.4
+ * Version 6.6.9.5
  * 11-MAY-2015
  * 
  * Automatic Update Information
- * <version_code>6.6.9.4</version_code>
+ * <version_code>6.6.9.5</version_code>
  */
 
 using System;
@@ -64,7 +64,7 @@ namespace PRoConEvents
     public class AdKats : PRoConPluginAPI, IPRoConPluginInterface
     {
         //Current Plugin Version
-        private const String PluginVersion = "6.6.9.4";
+        private const String PluginVersion = "6.6.9.5";
 
         public enum GameVersion
         {
@@ -6265,6 +6265,7 @@ namespace PRoConEvents
                             if ((UtcDbTime() - lastKeepAliveCheck).TotalSeconds > 30) {
                                 //Auto-squad-leader
                                 if (_isTestingAuthorized && 
+                                    _firstPlayerListComplete &&
                                     _serverInfo.ServerName.Contains("#7") &&
                                     _serverInfo.GetRoundElapsedTime().TotalMinutes > 5) {
                                     var onlinePlayers = _PlayerDictionary.Values.ToList();
