2f04c72a6452852935cd00ef12d516ee11a58fc7
ColColonCleaner <danielgradinjan@gmail.com>
2015-09-23 20:14:48 -0500

    Version 6.8.0.28

--- AdKats.cs
+++ AdKats.cs
@@ -20,11 +20,11 @@
  * Development by Daniel J. Gradinjan (ColColonCleaner)
  * 
  * AdKats.cs
- * Version 6.8.0.27
- * 22-SEP-2015
+ * Version 6.8.0.28
+ * 23-SEP-2015
  * 
  * Automatic Update Information
- * <version_code>6.8.0.27</version_code>
+ * <version_code>6.8.0.28</version_code>
  */
 
 using System;
@@ -64,7 +64,7 @@ namespace PRoConEvents
     public class AdKats : PRoConPluginAPI, IPRoConPluginInterface
     {
         //Current Plugin Version
-        private const String PluginVersion = "6.8.0.27";
+        private const String PluginVersion = "6.8.0.28";
 
         public enum GameVersion
         {
@@ -41025,7 +41025,9 @@ namespace PRoConEvents
                         //Double the wait duration if LRT is subscribed
                         waitTime.Add(_BattlelogWaitDuration);
                     }
-                    _threadMasterWaitHandle.WaitOne(waitTime);
+                    if (waitTime.TotalSeconds > 0) {
+                        _threadMasterWaitHandle.WaitOne(waitTime);
+                    }
                 }
                 _LastBattlelogAction = UtcNow();
             }
@@ -41046,7 +41048,9 @@ namespace PRoConEvents
                         }
                         waitTime = _IPAPIWaitDuration;
                     }
-                    _threadMasterWaitHandle.WaitOne(waitTime);
+                    if (waitTime.TotalSeconds > 0) {
+                        _threadMasterWaitHandle.WaitOne(waitTime);
+                    }
                 }
                 _LastIPAPIAction = UtcNow();
             } catch (Exception e) {
