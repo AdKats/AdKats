5bfd12a069d7e0d617ca1016f9525f3a64535aa7
ColColonCleaner <danielgradinjan@gmail.com>
2018-04-03 22:59:44 -0500

    Version 7.0.1.117

--- AdKats.cs
+++ AdKats.cs
@@ -20,11 +20,11 @@
  * Development by Daniel J. Gradinjan (ColColonCleaner)
  * 
  * AdKats.cs
- * Version 7.0.1.116
+ * Version 7.0.1.117
  * 3-APR-2018
  * 
  * Automatic Update Information
- * <version_code>7.0.1.116</version_code>
+ * <version_code>7.0.1.117</version_code>
  */
 
 using System;
@@ -66,7 +66,7 @@ namespace PRoConEvents
     public class AdKats :PRoConPluginAPI, IPRoConPluginInterface
     {
         //Current Plugin Version
-        private const String PluginVersion = "7.0.1.116";
+        private const String PluginVersion = "7.0.1.117";
 
         public enum GameVersionEnum
         {
@@ -16345,7 +16345,7 @@ namespace PRoConEvents
                         {
                             aKill.killer.ActiveChallenge.AddKill(aKill);
                         }
-                        if (aKill.victim.ActiveChallenge == null)
+                        if (aKill.victim.ActiveChallenge != null)
                         {
                             aKill.victim.ActiveChallenge.AddDeath(aKill);
                         }
@@ -16675,7 +16675,7 @@ namespace PRoConEvents
                         }
                     }
 
-                    if (aPlayer.ActiveChallenge == null)
+                    if (aPlayer.ActiveChallenge != null)
                     {
                         aPlayer.ActiveChallenge.AddSpawn(aPlayer);
                     }
