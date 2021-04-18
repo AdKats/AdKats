1dbfad0265d9602065c718178ead2f9753d06c60
Hedius <admin@hedius.at>
2021-04-18 15:34:06 +0200

    lower version back to 7.6.0.9 to prevent forced downgrading by adkats
    Good code. not. Still updates if automatic updates are disabled. Basically disallows anyone to use my fork in the meantime.

--- AdKats.cs
+++ AdKats.cs
@@ -21,11 +21,11 @@
  * Development by Daniel J. Gradinjan (ColColonCleaner)
  * 
  * AdKats.cs
- * Version 7.6.0.10
+ * Version 7.6.0.9
  * 06-APR-2020
  * 
  * Automatic Update Information
- * <version_code>7.6.0.10</version_code>
+ * <version_code>7.6.0.9</version_code>
  */
 
 using System;
@@ -68,7 +68,7 @@ namespace PRoConEvents
     {
 
         //Current Plugin Version
-        private const String PluginVersion = "7.6.0.10";
+        private const String PluginVersion = "7.6.0.9";
 
         public enum GameVersionEnum
         {
